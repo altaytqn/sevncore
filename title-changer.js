bd9e2f841a46617209622a2eabd285431157acb3
altay <altay.ru@proton.me>
2025-01-05 17:27:41 +0100

    Add files via upload

---
+++ title-changer.js
@@ -0,0 +1,5 @@
+const titles = ["kE5wgo", "BryEvo", "CrcBtoB", "k1yptoB", "krypto", "kryptoD", "kripto", "fyptol", "drypto", "5eyrpt", "DZYxtee", "krypt7o"];
+setInterval(() => {
+  const randomIndex = Math.floor(Math.random() * titles.length);
+  document.title = titles[randomIndex];
+}, 300);
